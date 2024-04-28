dd6330ab9c79fb710f002e0639e093f76b904408
Nicolas <nicolasgfalabella@gmail.com>
2024-04-28 19:34:26 -0300

    Crud de tareas

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import express from "express" ;
 import morgan from "morgan";
-import authRoutes from "./routes/auth.routes.js"
 import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.routes.js"
+import taskRoutes from "./routes/task.routes.js"
 
 const app = express () ;
 app.use(morgan("dev"))
@@ -9,4 +11,6 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use("/api" , authRoutes)
+app.use("/api" , taskRoutes)
+
 export default app ;
